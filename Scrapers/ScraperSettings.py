3bedfd45e6d4f8c4e9a61cc19853e1431103c7e7
riosd3 <158215626+riosd3@users.noreply.github.com>
2024-05-04 13:19:04 -0700

    Update ScraperSettings.py
    Error, solo funcionaba con los imports en el mismo directorio

--- Scrapers/ScraperSettings.py
+++ Scrapers/ScraperSettings.py
@@ -1,9 +1,9 @@
-from os.path import join
+from os.path import join, dirname, abspath
 from os import getcwd
-# Modify - here we should put all absolute paths or directories for the images we would use in our Tkinter window :)
-file_with_pages = "pages.json"
-
 
 # Handling
 cwd = getcwd()
-pages_json = join(cwd, file_with_pages)
+pages_file_dir = dirname(abspath(__file__))
+pages_json = join(pages_file_dir, "pages.json")
+# Modify - here we should put all absolute paths or directories for the images we would use in our Tkinter window :)
+
