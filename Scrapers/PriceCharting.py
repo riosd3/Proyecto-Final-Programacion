54766c4ca052be63bb19bb8d0a2b3871746a466e
OmarFloresQ <167242893+OmarFloresQ@users.noreply.github.com>
2024-05-17 00:31:04 -0700

    Update PriceCharting.py
    Actualizado.

--- Scrapers/PriceCharting.py
+++ Scrapers/PriceCharting.py
@@ -1,42 +1,69 @@
 import pandas as pd
-import requests
+from requests import Session
 from bs4 import BeautifulSoup
+from FilterCheck import pagesAvailability
+from selenium import webdriver
+from fake_useragent import UserAgent
+class PriceCharting:
+    ranWebAgent = UserAgent()
+    def __init__(self):
+        if not pagesAvailability["pricecharting"]["filtered"]:
+            self.main_url = pagesAvailability["pricecharting"]["main_url"]
+            options = webdriver.ChromeOptions()
+            options.add_argument(f"--useragent={PriceCharting.ranWebAgent.random}")
+            options.add_argument("--headless")
+            self.browser_selenium = webdriver.Chrome(options=options)
+            self.browser_requests = Session()
+            self.browser_requests.headers.update({"User-Agent":PriceCharting.ranWebAgent.random})
+            self.inst_status = True
+        else:
+            self.inst_status = False
 
-# URL de la página web a scrapear
-url = 'https://www.pricecharting.com/console/nes?sort=highest_price'
+    def getFilters(self):
+        r = self.browser_requests.get(self.main_url)
+        filter_tags = BeautifulSoup(r.content, "html.parser").find("ul", {"class":"menu-dropdown single"})
+        filter_info = []
+        for li_tags in filter_tags.find_all("li"):
+            opt_result = []
+            try:
+                current_filtrable = BeautifulSoup(str(li_tags), "html.parser")
+                title = current_filtrable.find("a", {"class":"small-desktop-hidden"}).get_text().strip().replace(" »", "")
+                for opt in current_filtrable:
+                    opt_filtrable = BeautifulSoup(str(opt), "html.parser")
+                    try:
+                        # This get the names from the menu
+                        console_name = opt_filtrable.find("a", {"class":"small-desktop-hidden"}).get_text().strip()
+                    except:
+                        print("Something fails at getFilters for PriceCharting we can not get category name")
+                    else:
+                        try:
+                            # Zero in the list to erase Japan and PAL (Europe)
+                            console_names_and_tags = BeautifulSoup(str(opt_filtrable.find_all("ul")[0]), "html.parser")
+                        except:
+                            print("Can find consoles for category: ", console_name)
+                        else:
+                            for li_console_info in console_names_and_tags.find_all("a"):
+                                try:
+                                    console_name = li_console_info.get_text().strip()
+                                    href = li_console_info.get("href")
+                                except:
+                                    print("Cant find console name and href for: ", console_name)
+                                else:
+                                    opt_result.append({"console_name":console_name, "href":href})
+                    print(opt_result)
+            except:
+                pass
+            else:
+                filter_info.append({"console_category":title, "filter_info":opt_result})
+        return filter_info
 
-# Realizar la solicitud GET a la URL
-response = requests.get(url)
 
-# Comprobar si la solicitud fue exitosa
-if response.status_code == 200:
-    # Parsear el contenido HTML
-    soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Lista para almacenar los datos
-    games_data = []
 
-    # Encontrar todos los elementos tr de la tabla
-    game_rows = soup.find_all('tr')
 
-    # Iterar sobre cada fila de la tabla
-    for row in game_rows:
-        # Obtener el nombre del juego
-        name = row.find('td', class_='title').a.text.strip()
-
-        # Obtener el precio
-        price = row.find('td', class_='price').span.text.strip()
-
-        # Agregar los datos a la lista
-        games_data.append({'Nombre del juego': name, 'Precio': price})
-
-    # Convertir la lista de datos en un DataFrame de Pandas
-    df = pd.DataFrame(games_data)
-
-    # Guardar el DataFrame en un archivo CSV
-    csv_file = 'pricecharting_games.csv'
-    df.to_csv(csv_file, index=False)
-
-    print("Scraping completado. Los datos han sido guardados en", csv_file)
-else:
-    print("Error al realizar la solicitud GET a la URL:", url)
+if __name__ == "__main__":
+    pricechar = PriceCharting()
+    print(pricechar.inst_status)
+    if pricechar.inst_status:
+        filters = pricechar.getFilters()
+        print(filters)
